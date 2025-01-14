efa511196115181d930dbba59b0fd72d57b54bab
antishchoolun <39624693+antishchoolun@users.noreply.github.com>
2025-01-14 16:45:26 +0400

    Update vidsrcsu.ts

--- src/providers/sources/vidsrcsu.ts
+++ src/providers/sources/vidsrcsu.ts
@@ -51,7 +51,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 }
 export const vidsrcsuScraper = makeSourcerer({
   id: 'vidsrcsu',
-  name: 'vidsrc.su (FlixHQ)',
+  name: 'vidsrc.sux (FlixHQ)',
   rank: 229,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
