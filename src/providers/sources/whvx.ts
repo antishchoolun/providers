b829e789243c58df74c87c8d4331bbce17d7a04a
antishchoolun <39624693+antishchoolun@users.noreply.github.com>
2025-01-14 15:38:24 +0400

    Update whvx.ts

--- src/providers/sources/whvx.ts
+++ src/providers/sources/whvx.ts
@@ -38,7 +38,7 @@ export const whvxScraper = makeSourcerer({
   id: 'whvx',
   name: 'VidBinge',
   rank: 270,
-  disabled: true,
+  disabled: false,
   externalSource: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
